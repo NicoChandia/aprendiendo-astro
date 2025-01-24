d5ea1fd16e53468df85afffe00180da4b38d8ad4
NicoChandia <nicolas.chandia18@gmail.com>
2025-01-23 22:56:44 -0300

    Server Islands ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig,envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server',
+  integrations: [tailwind()],
+  env:{
+    schema:{
+      //nos lee la variable de entorno y la tipea
+      //variables de entorno(cadenas de texto)
+      //transformo las mismas 
+      SHOW_BUY_BUTTON: envField.boolean({ context:'server', access:'public'}), //donde se puede leer
+      SCORE_API_ENDPOINT: envField.string({context: 'server', access: 'public'}),
+    }
+  }
 });
